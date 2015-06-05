0b3df5e5be8c48f9b56aaa74d27f464006cba6f9
Esteban Leon <eleon182@gmail.com>
2015-06-05 14:57:40 -0700

    api-route setup

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var request = require('request');
 
-var users = require('./server/users-route');
+var api = require('./server/api-routes');
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res, next) {
     res.render('index');
 });
-app.use('/api/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
