8dacc97d79f2aab8d47fdfc5f13c801a00fbc195
Esteban Leon <eleon182@gmail.com>
2015-06-16 07:13:04 -0700

    remove libs from version control

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,3 @@
 git log --pretty=oneline -1 > public/version.html
-aws s3 cp --recursive public s3://cobra-doodletech --cache-control max-age=30
+aws s3 sync public s3://cobra-doodletech --cache-control max-age=30
 rm public/version.html
