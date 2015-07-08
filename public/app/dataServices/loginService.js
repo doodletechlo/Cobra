dcd152e7974028bab3f67ac29e21308c022f1f18
Esteban Leon <eleon182@gmail.com>
2015-07-08 07:10:21 -0700

    switch to user service

--- public/app/dataServices/loginService.js
+++ public/app/dataServices/loginService.js
@@ -12,8 +12,8 @@ function loginService(serviceManager, $q) {
         var urlEncodedLogin = {
             username: username,
             password: password
-        };
-        serviceManager.makeRequest('login', 'POST', urlEncodedLogin).then(
+         };
+        serviceManager.makeRequest('user/login', 'POST', urlEncodedLogin).then(
             function success(response) {
                 serviceManager.setAuth(response.data.token);
                 deferred.resolve();
